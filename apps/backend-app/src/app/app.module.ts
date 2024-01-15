49828608cfa00411ce64ef8ddde5e682bddf76f5
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-01-15 12:22:12 +0530

    add auth module

--- apps/backend-app/src/app/app.module.ts
+++ apps/backend-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,7 +17,8 @@ import { UserModule } from './user/user.module';
       synchronize: true,
     }),
     UserModule,
+    AuthModule,
   ],
-  controllers: [AppController]
+  controllers: [AppController],
 })
 export class AppModule {}
---
+++ apps/backend-app/src/app/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (_configService: ConfigService) => ({
+        secret: _configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '100s',
+        },
+      }),
+    }),
+  ],
+  providers: [AuthService],
+})
+export class AuthModule {}
