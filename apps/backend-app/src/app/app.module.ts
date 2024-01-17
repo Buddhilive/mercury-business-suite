f7e444c0da48f51d6346bbeb7f7b930aa51ae53d
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-01-17 23:35:58 +0530

    update database connection

--- apps/backend-app/src/app/app.module.ts
+++ apps/backend-app/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (soliasConfigService: ConfigService) => ({
+        type: 'postgres',
+        host: soliasConfigService.get('DB_HOST'),
+        port: +soliasConfigService.get('DB_PORT'),
+        username: soliasConfigService.get('DB_USER'),
+        password: soliasConfigService.get('DB_PASS'),
+        database: soliasConfigService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      })
     }),
   ],
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule { }
