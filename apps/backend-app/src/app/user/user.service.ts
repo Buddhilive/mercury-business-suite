e713378c5868c6c4e27f98ae9ef213fea0f29f09
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-01-21 15:19:12 +0530

    create custom repository for users

--- apps/backend-app/src/app/user/user.service.ts
+++ apps/backend-app/src/app/user/user.service.ts
@@ -1,18 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { DeleteResult, Repository, UpdateResult } from 'typeorm';
-import { UserEntity } from './entities/user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Observable, from } from 'rxjs';
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { Observable, catchError, from } from 'rxjs';
 import { SearchUserDto } from './dto/search-user.dto';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository(UserEntity) private readonly userRepo: Repository<UserEntity>) {}
+  constructor(
+    private readonly userRepo: UsersRepository
+  ) { }
 
-  create(createUserDto: CreateUserDto): Observable<CreateUserDto> {
-    return from(this.userRepo.save(createUserDto));
+  async create(createUserDto: CreateUserDto): Promise<void> {
+    await this.userRepo.createUser(createUserDto);
   }
 
   findAll(): Observable<SearchUserDto[]> {
@@ -20,14 +21,26 @@ export class UserService {
   }
 
   findOne(id: number): Observable<SearchUserDto> {
-    return from(this.userRepo.findOne({ where: {id}}));
+    return from(this.userRepo.findOne({ where: { id } })).pipe(
+      catchError((err) => {
+        throw new NotFoundException(err);
+      })
+    );
   }
 
   update(id: number, updateUserDto: UpdateUserDto): Observable<UpdateResult> {
-    return from(this.userRepo.update(id, updateUserDto));
+    return from(this.userRepo.update(id, updateUserDto)).pipe(
+      catchError((err) => {
+        throw new NotFoundException(err);
+      })
+    );
   }
 
   remove(id: number): Observable<DeleteResult> {
-    return from(this.userRepo.delete(id));
+    return from(this.userRepo.delete(id)).pipe(
+      catchError((err) => {
+        throw new NotFoundException(err);
+      })
+    );
   }
 }
